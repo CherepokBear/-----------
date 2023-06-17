2ebef5e88838ecd71586706189fe8131cdbb8714
CherepokBear <sibgufk684@gmail.com>
2023-06-17 12:00:03 +0300

    Появилась надпись авторизации

--- renderComments.js
+++ renderComments.js
@@ -1,15 +1,15 @@
 
-import { sanitizeHtml} from "./utils.js";
-import { renderLogin} from "./renderLogin.js";
-import { delay} from "./utils.js";
-import { postComment} from "./api.js";
+import { sanitizeHtml } from "./utils.js";
+import { renderLogin } from "./renderLogin.js";
+import { delay } from "./utils.js";
+import { postComment } from "./api.js";
 
 
 export const renderComments = (
-    isInitiaLoading, 
-    comments, 
-    app, 
-    isPosting, 
+    isInitiaLoading,
+    comments,
+    app,
+    isPosting,
     user) => {
 
     const likeButtonClass = "like-button";
@@ -37,16 +37,15 @@ export const renderComments = (
         </li>`;
     })
         .join("");
-        
-        const appHtml = `
+
+    const appHtml = `
         <div class="container">
             <ul class = "comments">
-                ${isInitiaLoading ? "<div class = 'loading'>Загрузка коментариев...</div>" : commentsHTML }
+                ${isInitiaLoading ? "<div class = 'loading'>Загрузка коментариев...</div>" : commentsHTML}
             </ul>
 
-            ${
-                user ? 
-    `
+            ${user ?
+            `
         <div class="add-form">
             <h3 class="title">Форма входа</h3>
             <input type="text" class = 'add-form-name'  placeholder="Введите ваше имя" id="name-input" value=" ${user.name}" dasabled/>
@@ -56,50 +55,50 @@ export const renderComments = (
             <button class="add-button " id="add-form-button">Написать</button>
         </div>
     `
-            : ` <div class = "form-loading" style="margin-top: 20px>
+            : ` <div class = "form-loading" style="margin-top: 20px">
                     Что бы добавить комментарий, <a href='#' id="go-to-login" href='#'>авторизуйтесь</a>
                 </div> `
-             }
+        }
         </div>`;
 
-         app.innerHTML = appHtml;
-        if (!isInitiaLoading && !isPosting) {
-            for (const likeButton of document.querySelectorAll(`${likeButtonClass}`)) {
-                likeButton.addEventListener('click', (event) => {
-                    event.stopPropagation();
-                    const comment = comments[likeButton.dataset.index];
-                    comment.isLikeLoading = true;
+    app.innerHTML = appHtml;
+    if (!isInitiaLoading && !isPosting) {
+        for (const likeButton of document.querySelectorAll(`${likeButtonClass}`)) {
+            likeButton.addEventListener('click', (event) => {
+                event.stopPropagation();
+                const comment = comments[likeButton.dataset.index];
+                comment.isLikeLoading = true;
 
+                renderComments(isInitiaLoading, comments, app, isPosting);
+                delay(2000).then(() => {
+                    comment.likes = comment.isLiked ? comment.likes - 1 : comment.likes + 1;
+                    comment.isLiked = !comment.isLiked;
+                    comment.isLikedLoading = false;
                     renderComments(isInitiaLoading, comments, app, isPosting);
-                    delay(2000).then(() => {
-                        comment.likes = comment.isLiked ? comment.likes - 1 : comment.likes + 1;
-                        comment.isLiked = !comment.isLiked;
-                        comment.isLikedLoading = false;
-                        renderComments(isInitiaLoading, comments, app, isPosting);
-                    });
                 });
-            };
+            });
+        };
 
 
-    for (const comment of document.querySelectorAll(".comment")) {
-        comment.addEventListener('click', () => {
-            text.value = `%BEGIN_QUOTES${comments[commet.dataset.index].name}:
+        for (const comment of document.querySelectorAll(".comment")) {
+            comment.addEventListener('click', () => {
+                text.value = `%BEGIN_QUOTES${comments[commet.dataset.index].name}:
         ${comments[commet.dataset.index].text}END_QUOTE%
         `;
-        });
-    };
+            });
+        };
 
         if (!user) {
-            const goToLogin =  document.getElementById('go-to-login');
-            goToLogin.addEventListener("click", (event)=>{
+            const goToLogin = document.getElementById('go-to-login');
+            goToLogin.addEventListener("click", (event) => {
                 event.preventDefault();
                 renderLogin(isInitiaLoading, comments, app, isPosting)
             })
         }
 
         if (user) {
-            const addButton =  document.getElementById('add-button');
-            addButton.addEventListener("click", ()=>{
+            const addButton = document.getElementById('add-button');
+            addButton.addEventListener("click", () => {
                 const text = document.getElementById('text-input').value;
                 if (text) {
                     postComment(text, user.token).then((response) => {
